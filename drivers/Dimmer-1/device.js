bcd3f918d395141f63fe46bd9ab1b1ff94d0a2f4
limkopi78 <limkopi78@gmail.com>
2018-07-10 12:59:06 +0800

    Fixed missing on/off feature on UI

--- drivers/Dimmer-1/device.js
+++ drivers/Dimmer-1/device.js
@@ -13,6 +13,8 @@ class TouchPanelDimmer extends ZwaveDevice {
 
 		// register capabilities for this device
 		this.registerCapability('onoff', 'BASIC', {
+			get: 'BASIC_GET',
+			set: 'BASIC_SET',
 			report: 'BASIC_REPORT',
 			reportParser(report) {
 
