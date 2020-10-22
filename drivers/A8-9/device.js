cdc71525aed4c57d4aeac8da6ff5acd013f7026a
Ted Tolboom <TedTolboom@users.noreply.github.com>
2020-10-22 09:44:54 +0200

    Add support A8-9 sensor

--- drivers/A8-9/device.js
+++ drivers/A8-9/device.js
@@ -1,111 +1,111 @@
 'use strict';
 
 const Homey = require('homey');
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const { ZwaveDevice } = require('homey-meshdriver');
 
 class A89Sensor extends ZwaveDevice {
-	async onMeshInit() {
 
-		// enable debugging
-		this.enableDebug();
+  async onMeshInit() {
+    // enable debugging
+    // this.enableDebug();
 
-		// print the node's info to the console
-		this.printNode();
+    // print the node's info to the console
+    // this.printNode();
 
-		// register device capabilities
-		this.registerCapability('measure_co2', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-			report: 'SENSOR_MULTILEVEL_REPORT',
-			reportParser: report => {
-				if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-					if (report['Sensor Type'] === 'CO2-level (version 3)') {
-						this.setCapabilityValue('alarm_co2', report['Sensor Value (Parsed)'] >= (this.getSetting('CO2_notification') || 1200));
-						return report['Sensor Value (Parsed)'];
-					}
-				}
-				return null;
-			},
-		});
+    // register device capabilities
+    this.registerCapability('measure_co2', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+      report: 'SENSOR_MULTILEVEL_REPORT',
+      reportParser: report => {
+        if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
+          if (report['Sensor Type'] === 'CO2-level (version 3)') {
+            this.setCapabilityValue('alarm_co2', report['Sensor Value (Parsed)'] >= (this.getSetting('CO2_notification') || 1200));
+            return report['Sensor Value (Parsed)'];
+          }
+        }
+        return null;
+      },
+    });
 
-		// register device capabilities
-		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-		});
-		this.registerCapability('measure_humidity', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-		});
-		this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-		});
-		this.registerCapability('measure_noise', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-		});
-		this.registerCapability('alarm_motion', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-			report: 'SENSOR_MULTILEVEL_REPORT',
-			reportParser: report => {
-				if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-					if (report['Sensor Value (Parsed)'] === 1 && report['Sensor Type'] === 'General purpose value (version 1)') return true; 
-					if (report['Sensor Value (Parsed)'] === 0 && report['Sensor Type'] === 'General purpose value (version 1)') return false;
-				
-				return null;
-				}	
-			},
-		});
-		this.registerCapability('alarm_smoke', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-			report: 'SENSOR_MULTILEVEL_REPORT',
-			reportParser: report => {
-				if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-					if (report['Sensor Value (Parsed)'] === 100 && report['Sensor Type'] === 'Smoke Density (v8)') return true; 
-					if (report['Sensor Value (Parsed)'] === 0 && report['Sensor Type'] === 'Smoke Density (v8)') return false;
-				
-				return null;
-				}	
-			},
-		});
-		this.registerCapability('measure_voc', 'SENSOR_MULTILEVEL', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				//pollInterval: 'poll_interval' // maps to device settings
-			},
-			report: 'SENSOR_MULTILEVEL_REPORT',
-			reportParser: report => {
-				if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-					if (report['Sensor Type'] === 'Volatile Organic Compound (v7)') {
-						const parsedRAW = report['Sensor Value (Parsed)'];
-						const parsedVOC = Math.round(parsedRAW * 1000);
-						this.setCapabilityValue('alarm_voc', parsedVOC >= (this.getSetting('voc_notification') || 2200));
-						this.setCapabilityValue('measure_voc', parsedVOC);
-					}
-				}
-				return null;
-			},
-		});
+    // register device capabilities
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+    });
+    this.registerCapability('measure_humidity', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+    });
+    this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+    });
+    this.registerCapability('measure_noise', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+    });
+    this.registerCapability('alarm_motion', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+      report: 'SENSOR_MULTILEVEL_REPORT',
+      reportParser: report => {
+        if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
+          if (report['Sensor Value (Parsed)'] === 1 && report['Sensor Type'] === 'General purpose value (version 1)') return true;
+          if (report['Sensor Value (Parsed)'] === 0 && report['Sensor Type'] === 'General purpose value (version 1)') return false;
 
-		this.registerCapability('measure_pm25', 'SENSOR_MULTILEVEL', {
-			getOpts: {
+          return null;
+        }
+      },
+    });
+    this.registerCapability('alarm_smoke', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+      report: 'SENSOR_MULTILEVEL_REPORT',
+      reportParser: report => {
+        if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
+          if (report['Sensor Value (Parsed)'] === 100 && report['Sensor Type'] === 'Smoke Density (v8)') return true;
+          if (report['Sensor Value (Parsed)'] === 0 && report['Sensor Type'] === 'Smoke Density (v8)') return false;
+
+          return null;
+        }
+      },
+    });
+    this.registerCapability('measure_voc', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnStart: true, // get the initial value on app start
+        // pollInterval: 'poll_interval' // maps to device settings
+      },
+      report: 'SENSOR_MULTILEVEL_REPORT',
+      reportParser: report => {
+        if (report && report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
+          if (report['Sensor Type'] === 'Volatile Organic Compound (v7)') {
+            const parsedRAW = report['Sensor Value (Parsed)'];
+            const parsedVOC = Math.round(parsedRAW * 1000);
+            this.setCapabilityValue('alarm_voc', parsedVOC >= (this.getSetting('voc_notification') || 2200));
+            this.setCapabilityValue('measure_voc', parsedVOC);
+          }
+        }
+        return null;
+      },
+    });
+
+    this.registerCapability('measure_pm25', 'SENSOR_MULTILEVEL', {
+      getOpts: {
         		getOnStart: true, // get the initial value on app start
         // pollInterval: 'poll_interval' // maps to device settings
       },
@@ -124,7 +124,7 @@ class A89Sensor extends ZwaveDevice {
         return null;
       },
     });
-	}
+  }
 
 }
 
